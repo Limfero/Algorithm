2e76eec019f962784076d842d959a5c5a31a5e0b
TPAHCJIuT <limon4ik.mail@gmail.com>
2023-07-16 15:30:32 +0300

    Create new class Edge
    Contains the vertex of the start and end of the edge and the weight of the edge

---
+++ Graphs/Edge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class Edge
+    {
+        public int StartVertex { get; init; }
+        
+        public int EndVertex { get; init; }
+
+        public double Weight { get; init; }
+
+        public Edge(int startVertex, int endVertex, double weight)
+        {
+            StartVertex = startVertex;
+            EndVertex = endVertex;
+            Weight = weight;
+        }
+    }
+}
