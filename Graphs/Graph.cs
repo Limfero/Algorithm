14dc5db67a08a048851caf3e5d880e6544777e75
TPAHCJIuT <limon4ik.mail@gmail.com>
2023-07-16 04:44:14 +0300

    Create class Graph
    The class works with oriented acyclic graphs. It is able to add edges and perform topographic sorting of a graph

---
+++ Graphs/Graph.cs
@@ -0,0 +1,64 @@
+﻿namespace Graphs
+{
+    public class Graph
+    {
+        public Dictionary<int, Dictionary<int, double>> Edges { get; private set; }
+
+        private readonly string VertexDoesNotExist = "There is no such vertex";
+        private readonly string EdgeAlreadyExists = "Such a edge already exists!";
+
+        public Graph(int vertexCount)
+        {
+            Edges = new();
+
+            for (int i = 1; i <= vertexCount; i++)
+                Edges.Add(i, new());
+
+        }
+
+        public void AddEdge(int numberVertexWhereStarts, int numberVertexWhereEnds, double weight)
+        {
+            if (!Edges.ContainsKey(numberVertexWhereStarts) || !Edges.ContainsKey(numberVertexWhereEnds))
+                throw new Exception(VertexDoesNotExist);
+
+            foreach (var vertex in Edges[numberVertexWhereStarts].Keys)
+                if (vertex == numberVertexWhereEnds)
+                    throw new Exception(EdgeAlreadyExists);
+
+            Edges[numberVertexWhereStarts].Add(numberVertexWhereEnds, weight);
+        }
+
+        public List<int> TopologicalSort()
+        {
+            List<int> linearlyOrderedVertices = new();
+            int[] inDegree = new int[Edges.Count + 1];
+
+            for (int i = 0; i < Edges.Count; i++)
+                foreach(var vertex in Edges.ElementAt(i).Value.Keys)
+                    inDegree[vertex]++;
+
+            Stack<int> next = new();
+
+            for (int i = inDegree.Length - 1; i > 0; i--)
+                if (inDegree[i] == 0)
+                    next.Push(i);
+
+            while(next.Count != 0)
+            {
+                int u = next.Pop();
+
+                linearlyOrderedVertices.Add(u);
+
+                foreach (var vertex in Edges.ElementAt(u - 1).Value.Keys)
+                {
+                    inDegree[vertex]--;
+
+                    if (inDegree[vertex] == 0)
+                        next.Push(vertex);
+                }
+            }
+
+            return linearlyOrderedVertices;
+        }
+    }
+}
