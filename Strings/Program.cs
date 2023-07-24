f3e554ad67728ba74e2b249a2f563f15588f961d
TPAHCJIuT <limon4ik.mail@gmail.com>
2023-07-24 22:34:25 +0300

    Added a method for compiling transformation tables
    The method returns two two-dimensional arrays. The first one with the cost of operations, the second one with the name of the operation + refactoring

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -2,6 +2,11 @@
 {
     public class Program
     {
+        private readonly static string Delete = "del {0}";
+        private readonly static string Insert = "ins {0}";
+        private readonly static string Replace = "rep {0} by {1}";
+        private readonly static string Copy = "copy {0}";
+
         static void Main(string[] args)
         {
             string first = "CATCGA"; string second = "GTACCGTCA";
@@ -10,9 +15,27 @@
 
             string longestCommonSubsequence = AssembleLCS(first, second, LCSTable, first.Length, second.Length);
 
-            WriteTable(LCSTable);
+            first = "ACAAGC"; second = "CCGT";
+
+            List<Array> tables = ComputeTransformTables(first, second, -1, 1, 2, 2);
+
+            WriteLCSTable(LCSTable);
 
             Console.WriteLine(longestCommonSubsequence);
+
+            WriteTransformTables(tables);
+        }
+
+        private static void WriteTransformTables(List<Array> tables)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < tables[0].GetLength(0); i++)
+            {
+                for (int j = 0; j < tables[0].GetLength(1); j++)
+                    Console.Write("{0, -2} - {1, -13}", ((int[,])tables[0])[i, j], ((string[,])tables[1])[i, j]);
+
+                Console.WriteLine();
+            }
         }
 
         public static int[,] ComputeLCSTable(string first, string second)
@@ -42,7 +65,54 @@
                 return AssembleLCS(first, second, LCSTable, lengthFirst - 1, lengthSecond);
         }
 
-        private static void WriteTable(int[,] LCSTable)
+        private static List<Array> ComputeTransformTables(string first, string second, int copyingCost, int replacementCost, int removalСost, int insertCost)
+        {
+            InitializationCostAndOpArrays(first, second, removalСost, insertCost, out int[,] cost, out string[,] op);
+
+            SetMinimizedValueOfAllowableOperations(first, second, copyingCost, replacementCost, removalСost, insertCost, cost, op);
+
+            return new List<Array>() { cost, op};
+        }
+
+        private static void InitializationCostAndOpArrays(string first, string second, int removalСost, int insertCost, out int[,] cost, out string[,] op)
+        {
+            cost = new int[first.Length + 1, second.Length + 1];
+            op = new string[first.Length + 1, second.Length + 1];
+
+            for (int i = 1; i <= first.Length; i++)
+                ChangingArrayValues(cost, op, i, 0, i * removalСost, string.Format(Delete, first[i - 1]));
+
+            for (int i = 1; i <= second.Length; i++)
+                ChangingArrayValues(cost, op, 0, i, i * insertCost, string.Format(Insert, second[i - 1]));
+        }
+
+        private static void SetMinimizedValueOfAllowableOperations(string first, string second, int copyingCost, int replacementCost, int removalСost, int insertCost, int[,] cost, string[,] op)
+        {
+            for (int i = 1; i <= first.Length; i++)
+            {
+                for (int j = 1; j <= second.Length; j++)
+                {
+                    if (first[i - 1] == second[j - 1])
+                        ChangingArrayValues(cost, op, i, j, cost[i - 1, j - 1] + copyingCost, string.Format(Copy, first[i - 1]));
+                    else
+                        ChangingArrayValues(cost, op, i, j, cost[i - 1, j - 1] + replacementCost, string.Format(Replace, first[i - 1], second[j - 1]));
+
+                    if (cost[i - 1, j] + removalСost < cost[i, j])
+                        ChangingArrayValues(cost, op, i, j, cost[i - 1, j] + removalСost, string.Format(Delete, first[i - 1]));
+
+                    if (cost[i, j - 1] + insertCost < cost[i, j])
+                        ChangingArrayValues(cost, op, i, j, cost[i, j - 1] + insertCost, string.Format(Insert, second[j - 1]));
+                }
+            }
+        }
+
+        private static void ChangingArrayValues(int[,] cost, string[,] op, int startIndex, int endIndex, int newCost, string operation)
+        {
+            cost[startIndex, endIndex] = newCost;
+            op[startIndex, endIndex] = operation;
+        }
+
+        private static void WriteLCSTable(int[,] LCSTable)
         {
             for (int i = 0; i < LCSTable.GetLength(0); i++)
             {
